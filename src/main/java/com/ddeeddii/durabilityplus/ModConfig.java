ea335dd389132257b99dd49f38f9d120d04fad70
N4TH4NOT <n4th4not+git@gmail.com>
2025-10-14 23:31:21 +0200

    Add tooltips to config menu

--- src/main/java/com/ddeeddii/durabilityplus/ModConfig.java
+++ src/main/java/com/ddeeddii/durabilityplus/ModConfig.java
@@ -9,7 +9,9 @@ public class ModConfig
         implements ConfigData {
 
     @ConfigEntry.BoundedDiscrete(min = 5, max = 80)
+    @ConfigEntry.Gui.Tooltip
     public int yellow_style = 66;
     @ConfigEntry.BoundedDiscrete(min = 1, max = 40)
+    @ConfigEntry.Gui.Tooltip
     public int red_style = 33;
 }
